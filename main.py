0e293384ceebc9d225500e55e956baa7bfaddb80
Kamil Dydo <dydokamil@gmail.com>
2017-09-12 14:01:21 +0200

    Tuned hyperparameters

--- main.py
+++ main.py
@@ -7,24 +7,32 @@ from keras.layers import Dense, Flatten
 from keras.models import Sequential
 from keras.optimizers import Adam
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 OBSERVATION_SPACE = 4
-EPSILON_DECAY = .99
-GAMMA = .9
+EPSILON_DECAY = .9999
+SUCCEED_RENDER_THRESHOLD = 100  # how many times to succeed before rendering?
+GAMMA = .8
 
 epsilon = 1.
+render = False
+succeeded = 0
+
 D = deque(maxlen=5000)
-best = 0
 
 
 def get_model():
     model = Sequential()
-    model.add(Dense(16, activation='relu', input_shape=(4, 4)))
-    model.add(Flatten())
-    model.add(Dense(16, activation='relu'))
+    model.add(Flatten(input_shape=(1, 4, 4)))
+    # model.add(Convolution2D(256, 4, padding='same',
+    #                         activation='relu',
+    #                         input_shape=(1, 4, 4)))
+    # model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(2, activation='linear'))
     adam = Adam()
     model.compile(loss='mse', optimizer=adam)
+
     return model
 
 
@@ -60,7 +68,7 @@ def choose_action(s, model):
 
 
 if __name__ == '__main__':
-    env = gym.make("CartPole-v0")
+    env = gym.make("CartPole-v1")
     observation_space = env.observation_space.shape[0]
     action_space = env.action_space.n
 
@@ -71,21 +79,29 @@ if __name__ == '__main__':
         i = 0
         # fill transitions array with the same state
         s = np.stack((s, s, s, s))
+        s = s.reshape((1, 4, 4))
         terminated = False
         while not terminated:
+            if render:
+                env.render()
             a = choose_action(s, model)
             discount_epsilon()
             s_prime, r, terminated, _ = env.step(a)
 
             # add the new frame to the transition array
-            s_prime = np.vstack((s[1:], s_prime))
+            s_prime = np.vstack((s[0, 1:], s_prime))
+            s_prime = s_prime.reshape((1, 4, 4))
             save_exp_replay(s, a, r, s_prime, terminated)
             s = s_prime
             i += 1
 
-        if i > best:
-            best = i
-            print("Best:", best)
+        print("Survived", i, 'steps. Epsilon:', epsilon)
+        if i >= 200:
+            succeeded += 1
+            print('Succeeded', succeeded, 'times.')
+            if succeeded >= SUCCEED_RENDER_THRESHOLD:  # should render now?
+                epsilon = 0.
+                render = True
         # train the network
         batch = get_exp_replay()  # get BATCH_SIZE samples
         if batch is None:
@@ -108,4 +124,5 @@ if __name__ == '__main__':
             Q_sa[aa] = tt
             y[idx] = np.reshape(Q_sa, 2)
 
-            model.fit(ss, np.reshape(Q_sa, (1, 2)), verbose=0, epochs=1)
+        X = X.reshape((-1, 1, 4, 4))
+        model.train_on_batch(X, y)
